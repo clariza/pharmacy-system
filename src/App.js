9825c3d21b39d90c25b1580af1a5e9fc4c191aba
clary <clarysol77@gmail.com>
2025-09-18 12:18:31 -0400

    UPDATING COMPONENTS SALE,USERS,WAREHOUSE

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Layout } from 'antd';
+import FooterApp from './components/footer/footer';
+import Header from './components/header/header';
+import ContainerForgot from './containers/ContainerForgot';
+import Signin from './components/signin/signin';
 import './App.css';
+
+const { Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+<Router>
+      <Header />
+      <Layout>
+        <Content style={{ margin: '16px 16px' }} className="main-container-app">
+          <Routes>
+          <Route path="/" element={<Signin />} />
+          <Route path="/login" element={<Signin />} />
+           <Route path="/forgot" element={<ContainerForgot />} />
+           {/* <Route path="*" element={<ContainerSignin />} /> */}
+        </Routes>
+        </Content>
+        <FooterApp />
+      </Layout>
+      
+    </Router>
+    </Layout>
+    
   );
 }
 
